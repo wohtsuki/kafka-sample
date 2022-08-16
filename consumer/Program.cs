aec00bc2a8f60752c0ee5e6fe7a62ebb05a2f9eb
Walter <walter.ohtsuki-plu@kroton.parceirosedu.com.br>
2022-08-16 14:58:08 -0300

    string example

---
+++ consumer/Program.cs
@@ -0,0 +1,29 @@
+﻿using Confluent.Kafka;
+
+var consumerConfig = new ConsumerConfig
+{
+    BootstrapServers = "pkc-12pd3.brazilsouth.azure.confluent.cloud:9092",
+    SecurityProtocol = SecurityProtocol.SaslSsl,
+    SaslMechanism = SaslMechanism.Plain,
+    SaslUsername = "OWCJQBDQ2GNOQRAW",
+    SaslPassword = "vKizS6BMS3UoB8YW9d65xGsxrj1EEnNkEVvAEOtnP7HIi6uuTK7bbaxE9GizRqR8",
+    GroupId = "quantico.example",
+    AutoOffsetReset = AutoOffsetReset.Earliest,
+};
+
+string topic = "test1";
+
+using (var consumer = new ConsumerBuilder<string, string>(consumerConfig).Build())
+{
+    consumer.Subscribe(topic);
+
+    while(true)
+    {
+        var consumerResult = consumer.Consume();
+        var value = consumerResult.Message.Value;
+        Console.WriteLine($"Received user: {value}");
+        Thread.Sleep(500);
+    }
+}
+    
+
---
+++ producer/Program.cs
@@ -0,0 +1,32 @@
+﻿using Confluent.Kafka;
+
+var producerConfig = new ProducerConfig
+{
+    BootstrapServers = "pkc-12pd3.brazilsouth.azure.confluent.cloud:9092",
+    SecurityProtocol = SecurityProtocol.SaslSsl,
+    SaslMechanism = SaslMechanism.Plain,
+    SaslUsername = "OWCJQBDQ2GNOQRAW",
+    SaslPassword = "vKizS6BMS3UoB8YW9d65xGsxrj1EEnNkEVvAEOtnP7HIi6uuTK7bbaxE9GizRqR8"
+};
+
+
+
+
+var producer = new ProducerBuilder<string, string>(producerConfig).Build();
+string topic = "test1";
+
+
+
+while (true)
+{
+    string guidKey = Guid.NewGuid().ToString();
+    Message<string, string> msg = new()
+    {
+        Key = guidKey,
+        Value = Faker.Name.FirstName()
+    };
+    await producer.ProduceAsync(topic, msg);
+
+    Console.WriteLine($"key: {guidKey}, value: {msg.Value}");
+    Thread.Sleep(1000);
+}
